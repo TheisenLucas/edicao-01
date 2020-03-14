984b27ad0180f0bbe9bf3a5a95a4c68d1a036c8f
robsonrosa <robsonrosa@cwi.com.br>
2020-03-14 10:39:19 -0300

    aula 02 - conteúdo

---
+++ aula-02/src/heranca/App.java
@@ -0,0 +1,10 @@
+package heranca;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Submarino sub = new Submarino("Sub", 10.5);
+        System.out.println(sub.quantidadeRodas);
+    }
+}
